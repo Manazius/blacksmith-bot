b5d8b29a34a4675ad5de33511bfca486f648a134
Steven <manazius.org@gmail.com>
2015-10-21 16:55:14 +0500

    Create _source.py

---
+++ static/_source.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+
+# BlackSmith general configuration file
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+# Jabber server to connect
+SERVER = 'example.com'
+
+# Connecting Port
+PORT = 5222
+
+# Jabber server`s connecting Host
+HOST = 'example.com'
+
+# Using TLS (True - to enable, False - to disable)
+SECURE = True
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+# User`s account
+USERNAME = 'username'
+
+# Jabber ID`s Password
+PASSWORD = 'password'
+
+# Resourse (please don`t touch it)
+RESOURCE = u'simpleApps' # You can write unicode symbols here
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+# Default chatroom nick
+DEFAULT_NICK = u'BlackSmith-m.1' # You can write unicode symbols here
+
+# Groupchat message size limit
+CHAT_MSG_LIMIT = 1024
+
+# Private/Roster message size limit
+PRIV_MSG_LIMIT = 2024
+
+# Incoming message size limit
+INC_MSG_LIMIT = 8960
+
+# Working without rights of moder (True - to enable, False - to disable)
+MSERVE = False
+
+# Jabber account of bot`s owner
+BOSS = 'boss@example.com'
+
+# Memory usage limit (size in kilobytes, 0 - not limited)
+MEMORY_LIMIT = 49152
+
+# Admin password, used as a key to command "login"
+BOSS_PASS = ''
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
